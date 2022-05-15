5f1f308365d387e8be881c8df1ab886fbd71cf34
Muzahidul Islam <muzahidul190@gmail.com>
2022-05-15 10:59:02 +0600

    hytty

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ const client = new Client({
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("Tamim's PC! 😐", { type: 'PLAYING' });
-    client.user.setStatus('active');
+    client.user.setActivity("Muza's PC! 😐", { type: 'PLAYING' });
+    client.user.setStatus('idle');
 });
 
 
