6ebcb74e842f83a50ca916e96b8f35ec2ed61509
Muzahidul Islam <muzahidul190@gmail.com>
2022-05-16 02:16:51 +0600

    Merge branch 'main' of https://github.com/SMTamim/pcn-discord-bot

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,6 +4,9 @@ module.exports = {
     name: 'messageCreate',
     execute(message) {
         if (message.author.bot) {
+            if(message.content === `HADAHSDAHDHA`){
+                message.react('🎉');
+            }
             return;
         };
         console.log(`Message from ${message.author.username}: ${message.content}`);
--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,8 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Logged in as ${client.user.tag}!`);
-        client.user.setActivity("Tamim's PC! 😐", { type: 'PLAYING' });
-        client.user.setStatus('active');
+        client.user.setActivity("Muza's PC! 😐", { type: 'COMPETING' });
+        client.user.setStatus('idle');
         // console.log(client.commands);
         console.log("PCN is up and ready!");
     },
